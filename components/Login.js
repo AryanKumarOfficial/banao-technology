9b9d16400012492dfb2b700bea973a4775249aae
Aryan Kumar <Aryanak9163@gmail.com>
2024-06-23 18:02:41 +0530

    improves signin

--- components/Login.js
+++ components/Login.js
@@ -20,7 +20,7 @@ const LoginComponent = ({ className = "" }) => {
                 <div className="w-[90rem] h-[42.375rem] relative bg-purple-3 hidden max-w-full" />
                 <div className="w-[77.5rem] flex flex-row items-start justify-between max-w-full gap-[1.25rem] lg:flex-wrap">
                     <div className="w-[44.188rem] flex flex-col items-start justify-start gap-[2rem] min-w-[44.188rem] max-w-full lg:flex-1 mq750:gap-[1rem] mq1050:min-w-full">
-                        <h1 className="m-0 relative text-[2.25rem] tracking-[0.02em] inline-block italic font-semibold font-text-sm-bold text-left max-w-full z-[1] mq1050:text-[1.813rem] mq450:text-[1.375rem]">
+                        <h1 className="m-0 relative text-[2.25rem] tracking-[0.02em] inline-block italic font-semibold font-text-sm-bold text-left max-w-full z-[1] mq1050:text-[1.813rem] mq450:text-[1.375rem] mq450:tracking-tighter">
                             <span className="text-base-black">Explore your</span>
                             <span className="text-blue-1"> hobby</span>
                             <span className="text-base-black">{` or `}</span>
@@ -46,8 +46,8 @@ const LoginComponent = ({ className = "" }) => {
                     <div className="w-[25.625rem] flex flex-col items-start justify-start pt-[0.187rem] px-[0rem] pb-[0rem] box-border min-w-[25.625rem] max-w-full lg:flex-1 mq750:min-w-full">
                         <div className="self-stretch flex flex-col items-start justify-start gap-[2rem] mq450:gap-[1rem]">
                             <div className="w-[10.563rem] flex flex-row items-start justify-between gap-[1.25rem] z-[1]">
-                                <div className="w-[4.375rem] flex flex-col items-start justify-start gap-[0.125rem]">
-                                    <div className="relative text-[1.25rem] font-semibold font-text-sm-bold text-primary-full text-left inline-block min-w-[4.313rem] mq450:text-[1rem]">
+                                <div className="w-[4.375rem] mq750:mx-14 flex flex-col items-start justify-start gap-[0.125rem]">
+                                    <div className="relative text-[1.25rem] font-semibold font-text-sm-bold text-primary-full text-left inline-block min-w-[4.313rem] mq450:text-[1rem] hid">
                                         Sign In
                                     </div>
                                     <div className="self-stretch h-[0.125rem] relative bg-primary-full" />
@@ -59,8 +59,8 @@ const LoginComponent = ({ className = "" }) => {
                                     <div className="h-[0.125rem] w-[4.188rem] relative bg-primary-full hidden" />
                                 </div>
                             </div>
-                            <div className="self-stretch flex flex-col items-start justify-start gap-[1.75rem]">
-                                <div className="self-stretch flex flex-col items-start justify-start gap-[1rem]">
+                            <div className="self-stretch flex flex-col mq450:flex-col-reverse items-start justify-start gap-[1.75rem]">
+                                <div className="self-stretch flex flex-col items-start justify-start gap-[1rem] mq450:order-1">
                                     <button className="cursor-pointer py-[0.625rem] px-[1.5rem] bg-purple-3 self-stretch rounded-lg overflow-hidden flex flex-row items-start justify-start gap-[5.5rem] whitespace-nowrap z-[1] border-[1px] border-solid border-primary-full hover:bg-plum hover:text-white hover:box-border hover:border-[1px] hover:border-solid hover:border-plum mq450:gap-[2.75rem]">
                                         <img
                                             className="h-[1rem] w-[1rem] relative overflow-hidden shrink-0 min-h-[1rem]"
@@ -82,7 +82,7 @@ const LoginComponent = ({ className = "" }) => {
                                         </div>
                                     </button>
                                 </div>
-                                <div className="self-stretch flex flex-row items-start justify-start gap-[0.5rem] z-[1] mq450:flex-wrap">
+                                <div className="self-stretch flex flex-row items-start justify-start gap-[0.5rem] z-[1] mq450:flex-wrap mq450:order-2">
                                     <div className="flex-1 flex flex-col items-start justify-start pt-[0.5rem] px-[0rem] pb-[0rem] box-border min-w-[6.063rem]">
                                         <div className="self-stretch h-[0.125rem] relative bg-grey-light" />
                                     </div>
@@ -93,7 +93,7 @@ const LoginComponent = ({ className = "" }) => {
                                         <div className="self-stretch h-[0.125rem] relative bg-grey-light" />
                                     </div>
                                 </div>
-                                <div className="self-stretch flex flex-col items-start justify-start gap-[1rem]">
+                                <div className="self-stretch flex flex-col items-start justify-start gap-[1rem] mq450:order-5">
                                     <div className="self-stretch rounded-lg bg-grey-white overflow-hidden flex flex-row items-start justify-start py-2 px-3 z-[1] border border-solid border-grey-lighter focus-within:border-primary-full focus-within:ring focus-within:ring-purple-2">
                                         <input
                                             className="w-full border-none outline-none font-text-sm-bold text-[0.75rem] bg-transparent h-1 leading-4 text-darkgray text-left p-2  "
@@ -123,29 +123,58 @@ const LoginComponent = ({ className = "" }) => {
                                             />
                                         </button>
                                     </div>
+                                    <div className="w-[25.625rem] hidden flex-col items-start justify-start gap-[1.5rem] max-w-full mq450:flex">
+                                        <div className="self-stretch flex flex-row items-start justify-between gap-[1.25rem] mq450:flex-wrap">
+                                            <div className="flex flex-row items-start justify-start gap-[0.5rem]">
+                                                <input
+                                                    className="m-0 h-[1rem] w-[1rem] relative z-[1] accent-plum"
+                                                    type="checkbox"
+                                                    id="remember-me"
+                                                />
+                                                <label htmlFor="remember-me" className="relative text-[0.75rem] leading-[1rem] font-text-sm-bold text-plum font-semibold text-left inline-block min-w-[5.75rem] z-[1]">
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                            <Link href={'/forget'} className="flex flex-row items-start justify-start gap-[0.487rem] no-underline text-plum hover:text-primary-full">
+                                                <img
+                                                    className="h-[1rem] w-[0.988rem] relative overflow-hidden shrink-0 min-h-[1rem] z-[1]"
+                                                    alt=""
+                                                    src="/assets/lock_black.svg"
+                                                />
+                                                <div className="relative text-[0.75rem] leading-[1rem] font-text-sm-bold font-bold text-left inline-block min-w-[6.713rem] shrink-0 z-[1]">
+                                                    Forgot password?
+                                                </div>
+                                            </Link>
+                                        </div>
+                                        <button type="submit" className="cursor-pointer py-[0.625rem] px-[1.25rem] bg-[transparent] self-stretch rounded-lg overflow-hidden flex flex-row items-start justify-center z-[1] border-[1px] border-solid border-base-black hover:bg-[#8064a2] hover:!text-white hover:box-border hover:border-transparent transition-colors duration-500">
+                                            <div className="relative text-[0.875rem] leading-[1rem] font-semibold font-text-sm-bold  text-left inline-block min-w-[4.125rem]">
+                                                Continue
+                                            </div>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="w-[77.5rem] flex flex-row items-start justify-between max-w-full gap-[1.25rem] lg:flex-wrap">
-                    <div className="w-[43.569rem] flex flex-col items-start justify-start pt-[0.312rem] px-[0rem] pb-[0rem] box-border min-w-[43.569rem] max-w-full lg:flex-1 mq1050:min-w-full">
-                        <div className="self-stretch flex flex-row items-start justify-start gap-[0.9rem] max-w-full mq750:flex-wrap">
+                <div className="w-[77.5rem] flex flex-row mq450:flex--reverse items-start justify-between max-w-full gap-[1.25rem]  lg:flex-wrap mq450:order-4" >
+                    <div className="w-[43.569rem] flex flex-col items-start justify-start pt-[0.312rem] px-[0rem] pb-[0rem] box-border min-w-[43.569rem] max-w-full lg:flex-1 mq1050:min-w-full mq450:order-4">
+                        <div className="self-stretch flex flex-row items-start justify-start gap-[0.9rem] max-w-full mq750:flex-wrap mq450:flex-row">
                             <img
-                                className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full min-w-[13.688rem] min-h-[13.5rem] shrink-0 z-[1]"
+                                className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full min-w-[5rem] min-h-[13.5rem] shrink-0 z-[1]"
                                 loading="lazy"
                                 alt=""
                                 src="/assets/people1.svg"
                             />
                             <img
-                                className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full min-w-[14.063rem] min-h-[13.5rem] shrink-0 z-[1]"
+                                className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full min-w-[5rem] min-h-[13.5rem] shrink-0 z-[1]"
                                 loading="lazy"
                                 alt=""
                                 src="/assets/people2.svg"
                             />
                         </div>
                     </div>
-                    <div className="w-[25.625rem] flex flex-col items-start justify-start gap-[1.5rem] max-w-full">
+                    <div className="w-[25.625rem] flex flex-col items-start justify-start gap-[1.5rem] max-w-full mq450:hidden">
                         <div className="self-stretch flex flex-row items-start justify-between gap-[1.25rem] mq450:flex-wrap">
                             <div className="flex flex-row items-start justify-start gap-[0.5rem]">
                                 <input
